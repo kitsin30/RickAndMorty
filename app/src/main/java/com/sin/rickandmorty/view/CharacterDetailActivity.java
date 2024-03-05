435e68770ab696ea27652f4e6621442bbff811f6
kitsin30 <sinkit123@gmail.com>
2024-03-05 22:52:26 +0700

    add character fragment and character detail activity

---
+++ app/src/main/java/com/sin/rickandmorty/view/CharacterDetailActivity.java
@@ -0,0 +1,16 @@
+package com.sin.rickandmorty.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sin.rickandmorty.R;
+
+public class CharacterDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_detail);
+    }
+}
