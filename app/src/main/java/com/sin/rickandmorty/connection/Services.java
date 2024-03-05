f8827288444af447263d331e69429b2076e2f2ab
kitsin30 <sinkit123@gmail.com>
2024-03-05 22:22:48 +0700

    add string in indonesia

--- app/src/main/java/com/sin/rickandmorty/connection/Services.java
+++ app/src/main/java/com/sin/rickandmorty/connection/Services.java
@@ -1,9 +1,12 @@
 package com.sin.rickandmorty.connection;
 
+import com.sin.rickandmorty.response.CharacterResponse;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface Services {
-//    @GET("/character")
-//    Call
+    @GET("/character")
+    Call<CharacterResponse> getCharacter(@Query("page") String page);
 }
